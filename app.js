02843190988150855272ea016cdac560f0163bfc
toddlasley <todd.r.lasley@gmail.com>
2017-07-30 23:33:06 -0400

    routes for getting and creating pokemon complete. Moved most of db logic for route GET: /pokemon to mongo-util.js

--- app.js
+++ app.js
@@ -1,43 +1,46 @@
 var mongo = require('./mongo-util');
 var express = require('express');
 var bodyParser = require('body-parser');
-var parseUrlEncoded = bodyParser.urlencoded({extended: false});
 var port = 8080;
 
 var app = express();
 
 app.route('/pokemon')
     .get(function(req, res){        
-        var queryObject;
-        var filterObject;
-        if(Object.keys(req.query).length) {
-            queryObject = typeof(req.query.name) == undefined
-                ? { "name": req.query.name }
-                : { "type": req.query.type };
-            filterObject = {};        
-        }
-        else {
-            queryObject = {};
-            filterObject = {"name": true};
-        }
-        
         mongo.connect(function(db){
-                mongo.getPokemon(db).find(queryObject, filterObject)
-                    .sort({"name": 1 })
-                    .toArray(function(err, docs){
-                        if(err){
-                            res.status(500)
-                                .send('Failed to get Pokemon. Try again later')
-                                .end();
-                        }
-                        else {
-                            res.status(200)
-                                .json(docs)
-                                .end();
-                        }
-                        mongo.close(db);
-                    }); 
-            });
+            mongo.getPokemon(db, req.query, function(err, docs){
+                if(err){
+                    res.status(500)
+                        .send('Failed to get Pokemon. Try again later')
+                        .end();
+                }
+                else {
+                    res.status(200)
+                        .json(docs)
+                        .end();
+                }
+                mongo.close(db);
+            }); 
+        });
+    });
+
+app.route('/pokemon/add')
+    .put(bodyParser.json(), function(req, res){
+        mongo.connect(function(db){
+            mongo.addPokemon(db, req.body, function(pokemonAdded){
+                if(pokemonAdded){
+                    res.status(201)
+                        .send(req.body.name + ' added successfully!')
+                        .end();
+                }
+                else {
+                    res.status(500)
+                        .send('Failed to add pokemon.')
+                        .end();
+                }
+                mongo.close(db);
+            });                    
+        });
     });
 
 app.listen(port, function(){
--- mongo-util.js
+++ mongo-util.js
@@ -2,6 +2,8 @@ var client = require('mongodb').MongoClient;
 var url = 'mongodb://user:password@localhost:27017/test';
 
 module.exports = {
+    //database connect method
+    //callback will be whatever action is to be taken (read/create data)
     connect(callback) {
         client.connect(url, function(err, db){
             if(err) {
@@ -14,11 +16,34 @@ module.exports = {
             }
         });
     },
+    //database closing method
     close(db) {
         db.close();
         console.log('Closed connection to MongoDB instance.');
     },
-    getPokemon(db) {
-        return db.collection('pokemon');
+    getPokemon(db, queryParameters, callback) {
+        var queryObject;
+        var filterObject;
+        if(Object.keys(queryParameters).length) {
+            queryObject = queryParameters.name !== undefined
+                ? { "name": queryParameters.name }
+                : { "type": queryParameters.type };
+            filterObject = {};        
+        }
+        else {
+            queryObject = {};
+            filterObject = {"name": true};
+        }
+        db.collection('pokemon').find(queryObject, filterObject)
+            .sort({"name": 1 })
+            .toArray(function(err, docs){
+                callback(err, docs);                
+            });
+    },
+    addPokemon(db, doc, callback) {
+        db.collection('pokemon')
+            .insertOne(doc, function(err, result){
+                callback(result.insertedCount === 1);
+            });
     }
 }
