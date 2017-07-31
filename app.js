61ad5abd20561f5e2060258645cbb9468ec4be21
toddlasley <todd.r.lasley@gmail.com>
2017-07-30 22:00:45 -0400

    GET to /pokemon can get all pokemon or can use query paramerters Name or Type now

--- app.js
+++ app.js
@@ -7,22 +7,37 @@ var port = 8080;
 var app = express();
 
 app.route('/pokemon')
-    .get(function(req, res){
+    .get(function(req, res){        
+        var queryObject;
+        var filterObject;
+        if(Object.keys(req.query).length) {
+            queryObject = typeof(req.query.name) == undefined
+                ? { "name": req.query.name }
+                : { "type": req.query.type };
+            filterObject = {};        
+        }
+        else {
+            queryObject = {};
+            filterObject = {"name": true};
+        }
+        
         mongo.connect(function(db){
-            mongo.getPokemon(db).find({}, {'name': true}).toArray(function(err, docs){
-                if(err){
-                    res.status(500)
-                        .send('Failed to get Pokemon. Try again later')
-                        .end()
-                }
-                else {
-                    res.status(200)
-                        .json(docs)
-                        .end()
-                }
-                mongo.close(db);
-            }); 
-        });                
+                mongo.getPokemon(db).find(queryObject, filterObject)
+                    .sort({"name": 1 })
+                    .toArray(function(err, docs){
+                        if(err){
+                            res.status(500)
+                                .send('Failed to get Pokemon. Try again later')
+                                .end();
+                        }
+                        else {
+                            res.status(200)
+                                .json(docs)
+                                .end();
+                        }
+                        mongo.close(db);
+                    }); 
+            });
     });
 
 app.listen(port, function(){
