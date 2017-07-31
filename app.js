974acf5558412b329a4bbaa05b10a205a7406b61
toddlasley <todd.r.lasley@gmail.com>
2017-07-30 20:12:01 -0400

    app contains basic route for getting all pokemon names

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
+var mongo = require('./mongo-util');
 var express = require('express');
 var bodyParser = require('body-parser');
-var parseUrlEncoded = bodyParser.urlencoded({exended: false});
+var parseUrlEncoded = bodyParser.urlencoded({extended: false});
 var port = 8080;
 
 var app = express();
 
-app.route('/pokemon/:name')
+app.route('/pokemon')
     .get(function(req, res){
-
+        mongo.connect(function(db){
+            mongo.getPokemon(db).find({}, {'name': true}).toArray(function(err, docs){
+                if(err){
+                    res.status(500)
+                        .send('Failed to get Pokemon. Try again later')
+                        .end()
+                }
+                else {
+                    res.status(200)
+                        .json(docs)
+                        .end()
+                }
+                mongo.close(db);
+            }); 
+        });                
     });
 
 app.listen(port, function(){
