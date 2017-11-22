374acd5cd488b2d8e62ab33f03e7e44875eaca21
Todd <todd.r.lasley@gmail.com>
2017-11-22 12:10:34 -0500

    implementing CORS in use function()

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ var port = 8080;
 
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.route('/pokemon')
-    .get(function(req, res){        
+    .get(function(req, res){
         mongo.connect(function(db){
             mongo.getPokemon(db, req.query, function(err, docs){
-                res = setResponseHeaders(res, 'GET');
                 if(err){
                     res.status(500)
                         .send('Failed to get Pokemon. Try again later')
@@ -29,7 +35,6 @@ app.route('/pokemon/add')
     .put(bodyParser.json(), function(req, res){
         mongo.connect(function(db){
             mongo.addPokemon(db, req.body, function(pokemonAdded){
-                res = setResponseHeaders(res, 'PUT');
                 if(pokemonAdded){
                     res.status(201)
                         .send(req.body.name + ' added successfully!')
@@ -47,7 +52,6 @@ app.route('/pokemon/add')
 
 app.route('/pokemon/remove')
     .delete(bodyParser.json(), function(req, res){
-        res = setResponseHeaders(res, 'DELETE');
         if(req.body.id === undefined) {
             res.status(406)
                 .send('Please provide a valid JSON object with an "id" field.')
@@ -79,13 +83,4 @@ app.route('/pokemon/remove')
 
 app.listen(port, function(){
     console.log("Listening on port " + port);
-})
-
-var setResponseHeaders = function(res, method) {
-    res.set({
-        'Access-Control-Allow-Origin': 'http://localhost:4200',
-        'Access-Control-Allow-Methods': method,
-        'Access-Control-Allow-Headers': 'Content-Type'
-    });
-    return res;
-};
+});
